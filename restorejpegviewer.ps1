49bfa83248ec89413d8cd5b8257b70d0561dbbd1
James <39100243+James5618@users.noreply.github.com>
2023-09-29 16:26:38 +0100

    Add files via upload

---
+++ restorejpegviewer.ps1
@@ -0,0 +1,7 @@
+﻿Get-AppXPackage *WindowsStore* -AllUsers | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
+
+New-PSDrive -Name HKEY_CLASSES_ROOT -PSProvider Registry -Root HKEY_CLASSES_ROOT | Out-Null
+New-Item -Path $("HKEY_CLASSES_ROOT:\jpegfile\shell\open") -Force | Out-Null
+New-Item -Path $("HKEY_CLASSES_ROOT:\jpegfile\shell\open\command") -Force | Out-Null
+Set-ItemProperty -Path $("HKEY_CLASSES_ROOT:\jpegfile\shell\open") -Name "MuiVerb" -Type ExpandString -Value "@%ProgramFiles%\Windows Photo Viewer\PhotoViewer.dll,-3043"
+Set-ItemProperty -Path $("HKEY_CLASSES_ROOT:\jpegfile\shell\open\command") -Name "(Default)" -Type ExpandString -Value "%SystemRoot%\System32\rundll32.exe `"%ProgramFiles%\Windows Photo Viewer\PhotoViewer.dll`", ImageView_Fullscreen %1"
