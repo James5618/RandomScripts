2bc625e2530843f99ba0f0ae472ed3c4e6e84203
James <39100243+James5618@users.noreply.github.com>
2023-09-29 16:27:50 +0100

    Add files via upload

---
+++ AddEmailAliasADDS.ps1
@@ -0,0 +1,9 @@
+﻿Import-Csv "C:\temp\AddAlias\alias.csv" | ForEach-Object {
+    try{
+        Set-ADUser -Identity $_.samaccountname -add @{Proxyaddresses=$_.Proxyaddresses -split ";"} -ErrorAction Stop
+        Write-Host $_.samaccountname complete! -ForegroundColor Green
+    }catch{
+        Write-Host 'Failed : ' -NoNewline
+        Write-Host $_.samaccountname -ForegroundColor Red
+    }
+}
