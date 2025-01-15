b645edd762db55cdaff5d0ca7f623fb5f75fb39a
James <39100243+James5618@users.noreply.github.com>
2025-01-15 10:52:42 +0000

    Add files via upload

---
+++ DeployOutlookFontDefault.ps1
@@ -0,0 +1,9 @@
+﻿Connect-ExchangeOnline
+# Query all the mailboxes
+$Mailboxes = Get-Mailbox -ResultSize Unlimited
+ 
+# Loop through each mailbox
+foreach ($mailbox in $mailboxes) {
+    # Apply desired settings
+    Set-MailboxMessageConfiguration -Identity $mailbox.Identity -DefaultFontName "Gill Sans" -DefaultFontSize 11
+}
