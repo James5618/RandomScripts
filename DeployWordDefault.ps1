dc5d8ba2b30dc40b1a78ccc8b03dc6dedb10979a
James <39100243+James5618@users.noreply.github.com>
2025-01-15 10:50:49 +0000

    Add files via upload

---
+++ DeployWordDefault.ps1
@@ -0,0 +1,4 @@
+﻿$source = $PSScriptRoot
+$username = $Env:UserName
+cp -force "Normal.dotm" "C:\Users\$username\AppData\Roaming\Microsoft\Templates\"
+cp -force "NormalEmail.dotm" "C:\Users\$username\AppData\Roaming\Microsoft\Templates\"
