239192ef3168ddca067c5120a81c7aed96ca1b54
James <39100243+James5618@users.noreply.github.com>
2025-01-15 10:50:16 +0000

    Add files via upload

---
+++ AccesstoallCalendars.ps1
@@ -0,0 +1,28 @@
+﻿$userRequiringAccess = "emailaddress"
+$accessRight = "PublishingAuthor"
+ 
+$mailboxes = Get-mailbox
+$userRequiringAccess = Get-mailbox $userRequiringAccess
+foreach ($mailbox in $mailboxes) {
+    $accessRights = $null
+    $accessRights = Get-MailboxFolderPermission "$($mailbox.primarysmtpaddress):\calendar" -User $userRequiringAccess.PrimarySmtpAddress -erroraction SilentlyContinue
+         
+    if ($accessRights.accessRights -notmatch $accessRight -and $mailbox.primarysmtpaddress -notcontains $userRequiringAccess.primarysmtpaddress -and $mailbox.primarysmtpaddress -notmatch "DiscoverySearchMailbox") {
+        Write-Host "Adding or updating permissions for $($mailbox.primarysmtpaddress) Calendar" -ForegroundColor Yellow
+        try {
+            Add-MailboxFolderPermission "$($mailbox.primarysmtpaddress):\calendar" -User $userRequiringAccess.PrimarySmtpAddress -AccessRights $accessRight -ErrorAction SilentlyContinue    
+        }
+        catch {
+            Set-MailboxFolderPermission "$($mailbox.primarysmtpaddress):\calendar" -User $userRequiringAccess.PrimarySmtpAddress -AccessRights $accessRight -ErrorAction SilentlyContinue    
+        }        
+        $accessRights = Get-MailboxFolderPermission "$($mailbox.primarysmtpaddress):\calendar" -User $userRequiringAccess.PrimarySmtpAddress
+        if ($accessRights.accessRights -match $accessRight) {
+            Write-Host "Successfully added $accessRight permissions on $($mailbox.displayname)'s calendar for $($userrequiringaccess.displayname)" -ForegroundColor Green
+        }
+        else {
+            Write-Host "Could not add $accessRight permissions on $($mailbox.displayname)'s calendar for $($userrequiringaccess.displayname)" -ForegroundColor Red
+        }
+    }else{
+        Write-Host "Permission level already exists for $($userrequiringaccess.displayname) on $($mailbox.displayname)'s calendar" -foregroundColor Green
+    }
+}
